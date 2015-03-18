d5a8e7ba680d5426c9571d12855ac981e51b6d6c
Zehan Zhao <cnallenzhao@gmail.com>
2015-03-18 16:50:44 +0000

    Add something

--- db/migrate/20150317063537_add_team_member_to_issue.rb
+++ db/migrate/20150317063537_add_team_member_to_issue.rb
@@ -1,5 +1,6 @@
 class AddTeamMemberToIssue < ActiveRecord::Migration
   def change
+    remove_foreign_key :issues, :users
     remove_reference :issues, :user
     add_reference :issues, :team_member
   end
